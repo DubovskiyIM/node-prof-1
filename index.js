1623aef1a87f8cc289092d725d34ca3c87150ef0
IgnatDubovskiy <57371070+IgnatDubovskiy@users.noreply.github.com>
2020-10-16 13:26:35 +0300

    add 2 and 3 tasks

--- index.js
+++ index.js
@@ -1,15 +1,42 @@
 const express = require('express');
 const zlib = require('zlib');
+const request = require('request');
+const url = require('url');
+const { Transform } = require('stream');
 
 const PORT = 4321;
 
 const app = express();
+
+const transform = function (c, enc, cb) {
+  const result = c
+    .toString()
+    .split('')
+    .map(i => +i + 1)
+    .join('')
+  this.push(result);
+  cb();
+};
 
 app
   .use(express.static('public'))
   .post('/zip', (req, res) => {
     req
       .pipe(zlib.createGzip())
+      .pipe(res);
+  })
+  .get('/pipe', (req, res) => {
+    const r = request(url.format({
+      protocol: 'https',
+      host: 'kodaktor.ru',
+      pathname: '/g/forpipe'
+    }))
+    r.pipe(res);
+  })
+  .post('/plus1', (req, res) => {
+    let plusOne = new Transform({ transform });
+    req
+      .pipe(plusOne)
       .pipe(res);
   })
   .use((req, res) => {
